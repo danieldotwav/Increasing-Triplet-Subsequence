e03944b80ba232d32376109799404588744b6825
Daniel Rivas <drivas8891@gmail.com>
2024-02-04 12:51:48 -0800

    Added test cases

--- Source.cpp
+++ Source.cpp
@@ -2,18 +2,29 @@
 #include <vector>
 using namespace std;
 
-bool increasingTriplet(vector<int>& nums);
+bool increasingTriplet(const vector<int>& nums);
 
 int main() {
 	// Given an integer array nums, return true if there exists a triple 
 	// of indices (i, j, k) such that i < j < k and nums[i] < nums[j] < nums[k]. 
 	// If no such indices exists, return false.
 
+	// Case 1: Non-decreasing values
+	cout << "Test 1: [1, 2, 3, 4, 5]\nExpected: true\nActual: " <<
+		(increasingTriplet({ 1, 2, 3, 4, 5 }) == 1 ? "true\n\n" : "false\n\n");
+
+	// Case 2: Only decreasing values
+	cout << "Test 2: [5, 4, 3, 2, 1]\nExpected: false\nActual: " <<
+		(increasingTriplet({ 5, 4, 3, 2, 1 }) == 1 ? "true\n\n" : "false\n\n");
+
+	// Case 3: Negative values
+	cout << "Test 3: [2, 4, -2, -3]\nExpected: false\nActual: " <<
+		(increasingTriplet({ 2, 4, -2, -3 }) == 1 ? "true\n\n" : "false\n\n");
 
 	return 0;
 }
 
-bool increasingTriplet(vector<int>& nums) {
+bool increasingTriplet(const vector<int>& nums) {
 	int numsLength = nums.size();
 	if (numsLength < 3) { return false; }
 
